20d3217d20a12cffed67206fdb03d1f77f3d9bfd
Abdullah Al MahMud <abdudevs@gmail.com>
2023-06-08 18:11:42 +0600

    multer middleware added for file upload

---
+++ functions/GenerateImage.js
@@ -0,0 +1,33 @@
+const request = require("request");
+const options = {
+  method: "POST",
+  url: "https://stablediffusionapi.com/api/v3/text2img",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    key: "",
+    prompt:
+      "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner))",
+    negative_prompt: null,
+    width: "512",
+    height: "512",
+    samples: "1",
+    num_inference_steps: "20",
+    seed: null,
+    guidance_scale: 7.5,
+    safety_checker: "yes",
+    multi_lingual: "no",
+    panorama: "no",
+    self_attention: "no",
+    upscale: "no",
+    embeddings_model: "embeddings_model_id",
+    webhook: null,
+    track_id: null,
+  }),
+};
+
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
+});
---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const fs = require("fs");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" }); // Temporarily store the uploaded file
+const port = process.env.port || 5000;
+const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase/firebase.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: process.env.FIRE_STORAGE_BUCKET_NAME,
+});
+//firebase storage bucket
+const bucket = admin.storage().bucket();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_DB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const usersCollection = client.db("database").collection("usersCollection");
+    const imagesCollection = client
+      .db("database")
+      .collection("imagesCollection");
+    //endpoints
+
+    //users
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    //images
+    app.get("/images", async (req, res) => {
+      const query = {};
+      const cursor = imagesCollection.find(query);
+      const images = await cursor.toArray();
+      res.send(images);
+    });
+    app.post("/images", upload.single("image"), (req, res) => {
+      if (!req.file) {
+        res.status(400).json({ error: "No file uploaded" });
+        return;
+      }
+      console.log("file recieved");
+      const filePath = req.file.path; // Path to the temporarily stored file
+      const fileName = req.file.originalname; // Original name of the uploaded file
+
+      const destination = "uploads/" + fileName; // Destination path in Firebase Storage
+      console.log("starting upload");
+      bucket.upload(
+        filePath,
+        {
+          destination: destination,
+          metadata: {
+            contentType: req.file.mimetype, // Set the content type based on the uploaded file
+          },
+        },
+        (err, uploadedFile) => {
+          if (err) {
+            console.error("Error uploading file:", err);
+            res.status(500).json({ error: "Failed to upload file" });
+            return;
+          }
+
+          const imageUrl = uploadedFile.publicUrl();
+          console.log(imageUrl);
+
+          // Delete the temporarily stored file
+          fs.unlinkSync(filePath);
+          res.json({ imageUrl: imageUrl });
+        }
+      );
+
+      console.log("upload finished");
+    });
+    //main error and final log
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("computation completed");
+  }
+}
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  res.send("Hello and Welcome!!!");
+});
+
+app.listen(port, () => {
+  console.log("server running at full speed on port:", port);
+});
