94f53fef2a24c300ce78482ecbb2a53a54e71e8c
Abdullah Al MahMud <abdudevs@gmail.com>
2023-06-14 15:29:56 +0600

    added uploader

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,16 +1,23 @@
 // firebaseConfig.js
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-};
+const admin = require("firebase-admin");
+const { Storage } = require("@google-cloud/storage");
 
-module.exports = serviceAccount;
+//initialize firebase
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+});
+
+// Create a new instance of the Firebase Storage client
+const storage = new Storage({
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  credentials: {
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  },
+});
+
+module.exports = { storage };
--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const fs = require("fs");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" }); // Temporarily store the uploaded file
 const port = process.env.port || 5000;
-const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
-require("dotenv").config();
-
-const admin = require("firebase-admin");
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
-  storageBucket: process.env.FIRE_STORAGE_BUCKET_NAME,
-});
-
-// Firebase storage bucket
-const bucket = admin.storage().bucket();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { UploadFile } = require("./functions/UploadFile");
 
 app.use(cors());
 app.use(express.json());
@@ -61,46 +49,23 @@ async function run() {
           return res.status(400).json({ error: "No file uploaded" });
         }
         console.log("file received");
-        const filePath = req.file.path;
-        const fileName = req.file.originalname;
+        const file = req.file;
         const { author, city, state, zip } = req.body;
+        let data = { author, city, state, zip };
 
-        const destination = "uploads/" + fileName;
-        console.log("starting upload");
-        bucket.upload(
-          filePath,
-          {
-            destination: destination,
-            metadata: {
-              contentType: req.file.mimetype,
-            },
-          },
-          (err, uploadedFile) => {
-            if (err) {
-              console.error("Error uploading file:", err);
-              return res.status(500).json({ error: "Failed to upload file" });
-            }
-
-            const fileUrl = uploadedFile.publicUrl();
-            // Delete the temporarily stored file
-            fs.unlinkSync(filePath);
-            console.log(fileUrl);
-            const processedData = { author, city, state, zip, fileUrl };
-            imagesCollection
-              .insertOne(processedData)
-              .then((result) => {
-                console.log("upload finished");
-                res.json(result);
-              })
-              .catch((error) => {
-                console.error("Error inserting data:", error);
-                res.status(500).json({ error: "Failed to insert data" });
-              });
-          }
-        );
-      } catch (error) {
-        console.error("Error processing request:", error);
-        res.status(500).json({ error: "An error occurred" });
+        const folderName = "uploads69";
+        const fileUrl = await UploadFile(file, folderName);
+        const formattedData = {
+          ...data,
+          fileUrl,
+        };
+        const result = await imagesCollection.insertOne(formattedData);
+        res.send(result);
+        console.log(formattedData);
+        console.log(`image URL: ${fileUrl}`);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Failed to upload wallpaper");
       }
     });
 
